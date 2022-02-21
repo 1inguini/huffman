5a8a02e4832b3f037f54b48e5cfc73f4a2f41d5b
1inguini <9647142@gmail.com>
2022-02-21 22:35:40 +0900

    sort words by occurrences

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,29 @@
+use std::cmp::Ord;
 use std::collections::HashMap;
 use std::io;
 use std::io::Read;
 
-fn main() {
+fn main() -> Result<(), std::io::Error> {
+    // get words from stdin
     let mut input = String::new();
-    match io::stdin().read_to_string(&mut input) {
-        Ok(_) => {
-            let words = input.split_whitespace();
-            let mut words_occurrences: HashMap<&str, usize> = HashMap::new();
-            for word in words {
-                match &words_occurrences.get(&word) {
-                    None => words_occurrences.insert(word, 1),
-                    Some(&occurrences) => words_occurrences.insert(word, occurrences + 1),
-                };
-            }
-            println!("{:?}", words_occurrences);
-        }
-        Err(err) => println!("{}", err),
+    io::stdin().read_to_string(&mut input)?;
+    let words = input.split_whitespace();
+
+    // count occurrences of each word
+    let mut words_occurrences: HashMap<&str, usize> = HashMap::new();
+    for word in words {
+        match &words_occurrences.get(&word) {
+            None => words_occurrences.insert(word, 0),
+            Some(&occurrences) => words_occurrences.insert(word, occurrences + 1),
+        };
     }
+
+    // sort by occurences, from more to less
+    let mut words_occurrences: Vec<(&str, usize)> = words_occurrences
+        .into_iter()
+        .collect::<Vec<(&str, usize)>>();
+    words_occurrences.sort_by(|(_, v0), (_, v1)| Ord::cmp(v1, v0));
+
+    println!("{:?}", words_occurrences);
+    return Ok(());
 }

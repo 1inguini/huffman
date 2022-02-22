90056836d2f474505606be12cf10260dde36e59f
1inguini <9647142@gmail.com>
2022-02-22 12:57:52 +0900

    using usize wont work with many words

--- src/main.rs
+++ src/main.rs
@@ -10,100 +10,142 @@ enum Error {
     NoInput,
 }
 
-fn main() -> Result<(), Error> {
-    // utilities
-    #[derive(Debug, Clone)]
-    enum HuffTree<'a> {
-        Leaf(&'a str),
-        Node(Box<HuffTree<'a>>, &'a str),
+// utilities
+#[derive(Debug, Clone)]
+enum HuffTree<'a> {
+    Leaf(&'a str),
+    Node(Box<HuffTree<'a>>, &'a str),
+}
+
+impl<'a> HuffTree<'a> {
+    /// Create a new HuffTree, with the given Iterator of words
+    fn new<I>(words: &mut I) -> Option<HuffTree<'a>>
+    where
+        I: Iterator<Item = &'a str>,
+    {
+        fn helper<'a, I>(words_sorted_by_occurences: &mut I, rarest: HuffTree<'a>) -> HuffTree<'a>
+        where
+            I: Iterator<Item = &'a str>,
+        {
+            match words_sorted_by_occurences.next() {
+                None => rarest,
+                Some(rare) => helper(
+                    words_sorted_by_occurences,
+                    HuffTree::Node(Box::new(rarest), rare),
+                ),
+            }
+        }
+        // sort by occurences, from more to less
+        let mut words_occurrences = count_occurrences(words)
+            .into_iter()
+            .collect::<Vec<(&str, usize)>>();
+        words_occurrences.sort_by(|(_, v0), (_, v1)| Ord::cmp(v1, v0));
+        let mut words_occurrences = words_occurrences.into_iter().map(|(word, _)| word);
+        words_occurrences
+            .next()
+            .map(|rarest| helper(&mut words_occurrences, HuffTree::Leaf(rarest)))
     }
-    impl<'a> From<HuffTree<'a>> for HashMap<String, usize> {
-        fn from(tree: HuffTree) -> HashMap<String, usize> {
-            fn helper<'a>(
-                next: usize,
-                encodings: HuffTree<'a>,
-                map: &'a mut HashMap<String, usize>,
-            ) -> () {
-                match encodings {
-                    HuffTree::Leaf(word) => {
-                        map.insert(word.to_string(), next);
-                    }
-                    HuffTree::Node(rest, word) => {
-                        let prefix = next << 1;
-                        map.insert(word.to_string(), prefix);
-                        helper(prefix + 1, *rest, map);
-                    }
+    fn fold<F, T>(&self, initial: T, f: F) -> T
+    where
+        F: Fn(&str, T) -> T,
+    {
+        match self {
+            HuffTree::Leaf(word) => f(word, initial),
+            HuffTree::Node(rest, word) => rest.fold(f(word, initial), f),
+        }
+    }
+
+    /// format Hufftree to string with each word and corresponding encoding
+    fn format_encodings(&self) -> String {
+        fn helper(next: String, encodings: &HuffTree) -> String {
+            match encodings {
+                HuffTree::Leaf(word) => word.to_string() + "\t" + &next + "\n",
+                HuffTree::Node(rest, word) => {
+                    word.to_string() + "\t" + &next + "0" + "\n" + &helper(next + "1", rest)
                 }
             }
-            let mut map = HashMap::new();
-            helper(0, tree, &mut map);
-            map
         }
+        helper("".to_string(), self)
     }
-    fn into_huffman_coding<'a>(
-        words_occurrences: &mut Vec<&'a str>,
-        rarest: HuffTree<'a>,
-    ) -> HuffTree<'a> {
-        match words_occurrences.pop() {
-            None => rarest,
-            Some(rare) => {
-                into_huffman_coding(words_occurrences, HuffTree::Node(Box::new(rarest), rare))
-            }
-        }
-    }
-    // get words from stdin
-    let mut input = String::new();
-    io::stdin()
-        .read_to_string(&mut input)
-        .map_err(Error::IoError)?;
-    let words = input.split_whitespace();
-    // println!(
-    //     "get words from stdin:\n\t{:?}",
-    //     words.clone().collect::<Vec<&str>>()
-    // );
+}
 
-    // count occurrences of each word
+// usize overflows
+// impl<'a> From<HuffTree<'a>> for Vec<(String, Vec<bool>)> {
+//     fn from(tree: HuffTree) -> Vec<(String, Vec<bool>)> {
+//         fn helper<'a>(
+//             next: Vec<bool>,
+//             encodings: HuffTree<'a>,
+//             vec: &'a mut Vec<(String, Vec<bool>)>,
+//         ) -> () {
+//             match encodings {
+//                 HuffTree::Leaf(word) => {
+//                     vec.push((word.to_string(), next));
+//                 }
+//                 HuffTree::Node(rest, word) => {
+//                     let prefix = next << 1;
+//                     vec.push((word.to_string(), prefix));
+//                     helper(prefix + 1, *rest, vec);
+//                 }
+//             }
+//         }
+//         let mut vec = Vec::new();
+//         helper(0, tree, &mut vec);
+//         vec
+//     }
+// }
+
+// #[derive(Debug, Clone)]
+// struct HuffTreeIter<'a> {
+//     inner: Option<&'a HuffTree<'a>>,
+// }
+// impl<'a> Iterator for HuffTreeIter<'a> {
+//     type Item = &'a str;
+//     fn next(&mut self) -> Option<&'a str> {
+//         match self.inner.as_ref()? {
+//             HuffTree::Leaf(word) => {
+//                 *self = HuffTreeIter { inner: None };
+//                 Some(word)
+//             }
+//             HuffTree::Node(rest, word) => {
+//                 *self = HuffTreeIter { inner: Some(rest) };
+//                 Some(word)
+//             }
+//         }
+//     }
+// }
+// impl<'a> HuffTree<'a> {
+//     fn into_iter(&'a self) -> HuffTreeIter<'a> {
+//         HuffTreeIter { inner: Some(&self) }
+//     }
+// }
+
+/// count occurrences of each word
+fn count_occurrences<'a, I>(words: &mut I) -> HashMap<&'a str, usize>
+where
+    I: Iterator<Item = &'a str>,
+{
     let mut words_occurrences: HashMap<&str, usize> = HashMap::new();
-    for word in words.clone() {
+    for word in words {
         match &words_occurrences.get(&word) {
             None => words_occurrences.insert(word, 0),
             Some(&occurrences) => words_occurrences.insert(word, occurrences + 1),
         };
     }
-    // println!(
-    //     "count occurrences of each word:\n\t{:?}",
-    //     &words_occurrences
-    // );
+    words_occurrences
+}
 
-    // sort by occurences, from more to less
-    let mut words_occurrences: Vec<(&str, usize)> = words_occurrences
-        .into_iter()
-        .collect::<Vec<(&str, usize)>>();
-    words_occurrences.sort_by(|(_, v0), (_, v1)| Ord::cmp(v1, v0));
-    let mut words_occurrences: Vec<&str> = words_occurrences
-        .into_iter()
-        .map(|(word, _)| word)
-        .collect();
-    // println!(
-    //     "sort by occurences, from more to less:\n\t{:?}",
-    //     &words_occurrences
-    // );
-
+fn main() -> Result<(), Error> {
+    // get input from stdin
+    let mut input = String::new();
+    io::stdin()
+        .read_to_string(&mut input)
+        .map_err(Error::IoError)?;
     // derive the huffman encodings of words as a tree
-    let huffman_encoding: HuffTree = words_occurrences
-        .pop()
-        .map(|rarest| into_huffman_coding(&mut words_occurrences, HuffTree::Leaf(rarest)))
-        .ok_or(Error::NoInput)?;
-    // println!(
-    //     "derive the huffman encodings of words as a tree:\n\t{:?}",
-    //     &huffman_encoding
-    // );
+    let huffman_encoding: HuffTree =
+        HuffTree::new(&mut input.split_whitespace()).ok_or(Error::NoInput)?;
 
     // print each word and corresponding encoding
-    let huffman_encoding: HashMap<String, usize> = huffman_encoding.into();
-    for (word, encoding) in huffman_encoding.into_iter() {
-        println!("{}\n\t{:b}", &word, &encoding);
-    }
+    println!("{}", huffman_encoding.format_encodings());
 
     return Ok(());
 }
